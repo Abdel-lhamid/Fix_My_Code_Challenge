b3b49405adc2451423d50c940206daf5746d355b
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-05-19 12:42:08 +0100

    task1

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class Square():
+    def __init__(self, side=0):
+        self.side = side
+
+    def area(self):
+        """ Calculate the area of the square """
+        return self.side ** 2
+
+    def perimeter(self):
+        """ Calculate the perimeter of the square """
+        return 4 * self.side
+
+    def __str__(self):
+        return "Square(side={})".format(self.side)
+
+if __name__ == "__main__":
+    s = Square(side=12)
+    print(s)
+    print("Area:", s.area())
+    print("Perimeter:", s.perimeter())
