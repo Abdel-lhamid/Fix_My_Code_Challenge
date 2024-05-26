158aa4d0a6b9bd29de70dc90c9cff5bea6380b6b
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-05-26 15:47:38 +0100

    another approach

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,22 +2,24 @@
 
 class square():
     
-    def __init__(self, side_length):
-        self.side_length = side_length
+    width = 0
+    
+    def __init__(self, width = 0):
+        self.width = width
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.side_length ** 2
+        return self.width * self.width
 
-    def perimeter_of_my_square(self):
-        return 4 * self.side_length
+    def PermiterOfMySquare(self):
+        return (self.width * 4)
 
     def __str__(self):
-        return "{} by {} square".format(self.side_length, self.side_length)
+        return "{}".format(self.width)
 
 if __name__ == "__main__":
-    s = Square(side_length=12)
+
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
-
+    print(s.PermiterOfMySquare())
